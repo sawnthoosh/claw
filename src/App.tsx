4d7607017c2929e6a9cb08d88d724a47cb264e6e
sawnthoosh <santgolla9@gmail.com>
2026-04-04 20:15:59 +0530

    Refactor App component to use Login and ChatApp

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,15 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import FloatingChat from './components/FloatingChat';
-import Home from './pages/Home';
-import Topics from './pages/Topics';
-import CategoryView from './pages/CategoryView'; // <-- Add this import
+import Login from './pages/Login';
+import ChatApp from './pages/ChatApp';
 
 function App() {
-  return (
-    <div className="min-h-screen flex flex-col bg-gray-50 font-sans text-gray-900">
-      <Navbar />
-      
-      <main className="flex-1">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/topics" element={<Topics />} />
-          {/* Add the dynamic route for individual categories */}
-          <Route path="/topics/:slug" element={<CategoryView />} />
-        </Routes>
-      </main>
-
-      <FloatingChat />
-    </div>
-  );
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/chat" element={<ChatApp />} />
+    </Routes>
+  );
 }
 
 export default App;
