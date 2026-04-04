868bbb8d410cb860f630139eeed0c9f83f804690
sawnthoosh <santgolla9@gmail.com>
2026-04-04 20:32:57 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+// src/App.tsx
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
 import Login from './pages/Login';
 import ChatApp from './pages/ChatApp';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/chat" element={<ChatApp />} />
     </Routes>
   );
