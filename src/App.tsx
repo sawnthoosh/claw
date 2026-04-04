9236c9dcdaf0b2672315ac3102f1e4edfb649783
sawnthoosh <santgolla9@gmail.com>
2026-04-04 19:03:24 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,25 @@ import Navbar from './components/Navbar';
 import FloatingChat from './components/FloatingChat';
 import Home from './pages/Home';
 import Topics from './pages/Topics';
+import CategoryView from './pages/CategoryView'; // <-- Add this import
 
 function App() {
-  return (
-    <div className="min-h-screen flex flex-col bg-gray-50 font-sans text-gray-900">
-      <Navbar />
-      
-      <main className="flex-1">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/topics" element={<Topics />} />
-        </Routes>
-      </main>
+  return (
+    <div className="min-h-screen flex flex-col bg-gray-50 font-sans text-gray-900">
+      <Navbar />
+      
+      <main className="flex-1">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/topics" element={<Topics />} />
+          {/* Add the dynamic route for individual categories */}
+          <Route path="/topics/:slug" element={<CategoryView />} />
+        </Routes>
+      </main>
 
-      <FloatingChat />
-    </div>
-  );
+      <FloatingChat />
+    </div>
+  );
 }
 
 export default App;
